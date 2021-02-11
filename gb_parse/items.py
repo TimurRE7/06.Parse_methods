7ee8b551540da284126f268d1b6c205be5367186
TimurRE <timurre7@gmail.com>
2021-02-11 04:43:55 +0300

    Lesson 5 homework

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -6,7 +6,33 @@
 import scrapy
 
 
-class GbParseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# class GbParseItem(scrapy.Item):
+#     # define the fields for your item here like:
+#     # name = scrapy.Field()
+#     pass
+
+
+class AutoyoulaItem(scrapy.Item):
+    _id = scrapy.Field()
+    title = scrapy.Field()
+    images = scrapy.Field()
+    description = scrapy.Field()
+    url = scrapy.Field()
+    author = scrapy.Field()
+    specifications = scrapy.Field()
+    price = scrapy.Field()
+    test = scrapy.Field()
+
+
+class HHVacancyItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    salary = scrapy.Field()
+    description = scrapy.Field()
+    skills = scrapy.Field()
+    company_url = scrapy.Field()
+    company_name = scrapy.Field()
+    company_site = scrapy.Field()
+    company_sphere = scrapy.Field()
+    company_description = scrapy.Field()
---
+++ gb_parse/loaders.py
@@ -0,0 +1,76 @@
+import re
+from urllib.parse import urljoin
+from scrapy import Selector
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from .items import AutoyoulaItem, HHVacancyItem
+
+
+# def clear_price(item: str):
+#     try:
+#         return float(item.replace("\u2009", ''))
+#     except ValueError:
+#         return None
+
+
+# def get_author(item):
+#     re_str = re.compile(r"youlaId%22%2C%22([0-9|a-zA-Z]+)%22%2C%22avatar")
+#     result = re.findall(re_str, item)
+#     return urljoin("https://youla.ru/user/", result[0]) if result else None
+
+
+# def get_description(items):
+#     return '\n'.join(items)
+#
+#
+# def get_specifications(data):
+#     tag = Selector(text=data)
+#     name = tag.xpath('//div[contains(@class, "AdvertSpecs_label")]/text()').get()
+#     value = tag.xpath('//div[contains(@class, "AdvertSpecs_data")]//text()').get()
+#     return {name: value}
+#
+#
+# def specifications_out(data):
+#     result = {}
+#     for itm in data:
+#         result.update(itm)
+#     return result
+
+
+# class AutoyoulaLoader(ItemLoader):
+#     default_item_class = AutoyoulaItem
+#     url_out = TakeFirst()
+#     title_out = TakeFirst()
+#     price_in = MapCompose(clear_price)
+#     price_out = TakeFirst()
+#     author_in = MapCompose(get_author)
+#     author_out = TakeFirst()
+#     description_out = get_description
+#     specifications_in = MapCompose(get_specifications)
+#     specifications_out = specifications_out
+
+
+def clear_data(data):
+    try:
+        return data.replace("\xa0", '')
+    except ValueError:
+        return None
+
+
+def get_sphere(data: str):
+    return data.title().split(', ')
+
+
+class HHVacancyLoader(ItemLoader):
+    default_item_class = HHVacancyItem
+    title_out = TakeFirst()
+    url_out = TakeFirst()
+    description_in = ''.join
+    description_out = TakeFirst()
+    salary_in = ''.join
+    salary_out = MapCompose(clear_data)
+    company_sphere_out = MapCompose(get_sphere)
+    company_description_in = ''.join
+    company_description_out = MapCompose(clear_data)
+    company_name_in = ' '.join
+    company_name_out = MapCompose(clear_data)
